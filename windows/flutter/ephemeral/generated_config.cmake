ca8869b5e76782d2fca6b69c6d6cf3fd2eedc462
Tadeu Torquato <tadeu@infoconsultor.info>
2022-01-23 17:58:24 -0300

    gitignore e register

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\App\\tadeu\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Projetos\\Flutter\\PensamentoDoDia" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\App\\tadeu\\flutter"
+  "PROJECT_DIR=C:\\Projetos\\Flutter\\PensamentoDoDia"
+  "FLUTTER_ROOT=C:\\App\\tadeu\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Projetos\\Flutter\\PensamentoDoDia\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Projetos\\Flutter\\PensamentoDoDia"
+  "FLUTTER_TARGET=C:\\Projetos\\Flutter\\PensamentoDoDia\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Projetos\\Flutter\\PensamentoDoDia\\.dart_tool\\package_config.json"
+)
